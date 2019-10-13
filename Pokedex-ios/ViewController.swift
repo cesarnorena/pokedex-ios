21af048ac06cc71ffe3f8b946cae0bed305e2c9d
cesarnorena <cesar.norena@nubank.com.br>
2019-10-13 01:14:57 -0300

    Initial Commit

---
+++ Pokedex-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex-ios
+//
+//  Created by César Noreña on 13/10/19.
+//  Copyright © 2019 César Noreña. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Pokedex-iosTests/Pokedex_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  Pokedex_iosTests.swift
+//  Pokedex-iosTests
+//
+//  Created by César Noreña on 13/10/19.
+//  Copyright © 2019 César Noreña. All rights reserved.
+//
+
+import XCTest
+@testable import Pokedex_ios
+
+class Pokedex_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
